12121e505a50f05f5e974ec6a0359b2a6e9a32a4
samuelpondugala <samueldavid.webdev@gmail.com>
2025-05-28 00:16:59 +0530

    feat: implement conditional rendering for Navbar based on route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,29 @@ import Register from './components/Register.jsx'
 import Login from './components/Login.jsx'
 import Navbar from './components/Navbar.jsx'
 import About from './components/About.jsx'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 
 function App() {
-
+  const location = useLocation()
+  const noNavbar = location.pathname === "/register" || location.pathname === "/"
   return (
     <>
-    <Navbar 
+    {
+      noNavbar ?
+      <Routes>
+        <Route path = "/" element = {<Login/>}/>
+        <Route path = "/register" element = {<Register/>}/>
+      </Routes>
+      :
+      <Navbar 
       content = {
       <Routes>
-        <Route path="/" element={<Login/>} />
         <Route path="/home" element={<Home/>} />
-        <Route path="/register" element={<Register/>}/>
         <Route path = "/about" element={<About/>} />
       </Routes>
       }
     />
-      
+    }
     </>
   )
 }
