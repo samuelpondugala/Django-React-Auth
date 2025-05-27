63616d94baf703cd304fde9026084d1f21814430
David Samuel <davidsamuel7143@gmail.com>
2025-05-27 20:42:49 +0530

    Add CORS support and update middleware in settings.py

--- backend/auth/settings.py
+++ backend/auth/settings.py
@@ -26,11 +26,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # Django REST Framework
+    'corsheaders',  # CORS headers for API
     'users', # Custom user app
     
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # CORS middleware
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -39,6 +41,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173', ]
 
 ROOT_URLCONF = 'auth.urls'
 
